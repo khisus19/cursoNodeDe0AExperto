4cced73d388d343e15375440ec5929e98926d9b3
Khisus19 <khisus19@gmail.com>
2022-12-15 09:13:59 -0600

    Promise chaining

--- 02-fundamentos/promesas.js
+++ 02-fundamentos/promesas.js
@@ -52,11 +52,21 @@ const getSalario = (id) => {
 
 const id = 3 ;
 
-getEmpleado(id)
+/* getEmpleado(id)
   .then( empleado => console.log(empleado))
   .catch( err => console.log(err))
   
   
 getSalario(id)
   .then( salario => console.log(salario))
-  .catch( err => console.log(err))
+  .catch( err => console.log(err)) */
+
+let nombre
+
+getEmpleado(id)
+  .then( empleado => {
+    nombre = empleado;
+    return getSalario(id)
+  })
+  .then( salario => console.log(`El empleado con el nombre ${nombre} tiene un salario de: ${salario}`))
+  .catch(err => console.log(err));
