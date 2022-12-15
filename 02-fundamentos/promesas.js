c8844904074c0f48a9f2fd4e8ca22b2b355f7383
Khisus19 <khisus19@gmail.com>
2022-12-15 09:04:25 -0600

    Promises

---
+++ 02-fundamentos/promesas.js
@@ -0,0 +1,62 @@
+const empleados = [
+  {
+    id: 1,
+    nombre: "Fernando"
+  },
+  {
+    id: 2,
+    nombre: "Linda"
+  },
+  {
+    id: 3,
+    nombre: "Karen"
+  }
+];
+
+const salarios = [
+  {
+    id: 1,
+    salario: 1000
+  },
+  {
+    id: 2,
+    salario: 1500
+  }
+];
+
+
+const getEmpleado = ( id ) => {
+
+  const promesa = new Promise((resolve, reject) => {
+
+    const empleado = empleados.find( e => e.id === id)?.nombre;
+    
+    (empleado)
+      ? resolve(empleado)
+      : reject(`No existe el empleado con el id ${id}`);
+  });
+  return promesa;
+}
+
+const getSalario = (id) => {
+
+  const salario = salarios.find(s => s.id === id)?.salario;
+
+  const promesa = new Promise((resolve, reject) => {
+    salario 
+      ? resolve(salario)
+      : reject(`No tenemos el salario de la id ${id}`);
+  })
+  return promesa
+}
+
+const id = 3 ;
+
+getEmpleado(id)
+  .then( empleado => console.log(empleado))
+  .catch( err => console.log(err))
+  
+  
+getSalario(id)
+  .then( salario => console.log(salario))
+  .catch( err => console.log(err))
