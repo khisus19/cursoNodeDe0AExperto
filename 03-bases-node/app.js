19e6c3001dc20d26b6e2a9d8e14c38cf152b6b74
Khisus19 <khisus19@gmail.com>
2022-12-15 21:13:43 -0600

    Creando un proyecto de node e instalar yargs package

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,19 +1,23 @@
 const { crearArchivoDeMultiplicar } = require("./helpers/multiplicar");
+const argv = require("yargs").argv;
+
 
 console.clear();
 
 // console.log(process.argv);
 
-const [ , , arg3 = "base=5"] = process.argv;
-const [ , base = 5 ] = arg3.split("=");
-console.log( base );
+console.log(process.argv);
+console.log(argv);
+
+// const [ , , arg3 = "base=5"] = process.argv;
+// const [ , base = 5 ] = arg3.split("=");
 
 
 // const base = 6;
 
-crearArchivoDeMultiplicar(base)
-  .then(nombreArchivo => console.log(nombreArchivo, "creado"))
-  .catch(err => console.log(err))
+// crearArchivoDeMultiplicar(base)
+//   .then(nombreArchivo => console.log(nombreArchivo, "creado"))
+//   .catch(err => console.log(err))
 
 
 
