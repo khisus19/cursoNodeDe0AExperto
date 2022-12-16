ffccab7a7615ea8f2b2d38ffe5ee2148dd317e31
Khisus19 <khisus19@gmail.com>
2022-12-15 22:11:15 -0600

    Dar opciones a yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,23 +1,35 @@
+const { demandOption, options, boolean } = require("yargs");
 const { crearArchivoDeMultiplicar } = require("./helpers/multiplicar");
-const argv = require("yargs").argv;
+const argv = require("yargs")
+              .option("b", {
+                alias: "base",
+                type: "number",
+                demandOption: true
+              })
+              .option("l", {
+                alias: "listar",
+                type: "boolean",
+                demandOption: false,
+                default: false
+              })
+              .check( (argv, options) => {
+                if( isNaN(argv.b)) {
+                  throw "La base debe ser numerica"
+                }
+                return true;
+              })
+              .argv;
 
 
 console.clear();
 
-// console.log(process.argv);
 
-console.log(process.argv);
-console.log(argv);
-
-// const [ , , arg3 = "base=5"] = process.argv;
-// const [ , base = 5 ] = arg3.split("=");
+// console.log(argv);
 
 
-// const base = 6;
-
-// crearArchivoDeMultiplicar(base)
-//   .then(nombreArchivo => console.log(nombreArchivo, "creado"))
-//   .catch(err => console.log(err))
+crearArchivoDeMultiplicar(argv.b, argv.l)
+  .then(nombreArchivo => console.log(nombreArchivo))
+  .catch(err => console.log(err))
 
 
 
--- 03-bases-node/helpers/multiplicar.js
+++ 03-bases-node/helpers/multiplicar.js
@@ -1,21 +1,24 @@
 const fs = require("fs");
 let salida = "";
 
-const crearArchivoDeMultiplicar = async (base = 5) => {
+const crearArchivoDeMultiplicar = async (base = 5, listar) => {
 
   try {
-    console.log("==============================");
-    console.log(`      Tabla del: ${base}`);
-    console.log("==============================");
-
     for (let i = 1; i < 11; i++) {
       salida += `${base} x ${i} = ${base * i}\n`;
     }
-    console.log(salida);
+
+    if(listar) {
+      console.log("==============================");
+      console.log(`      Tabla del: ${base}`);
+      console.log("==============================");
+      console.log(salida);
+    }
+
     fs.writeFileSync(`../03-bases-node/tabla-${base}.txt`, salida);
 
 
-    return `./03-bases-node/tabla-${base}.txt creado`;
+    return `Archivo tabla-${base}.txt creado`;
   } catch (error) {
     throw error
   }
