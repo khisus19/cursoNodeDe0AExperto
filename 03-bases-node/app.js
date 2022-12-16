e9b8b1e8603a42ee94cc907ca42ad75ad0b03538
Khisus19 <khisus19@gmail.com>
2022-12-15 19:54:24 -0600

    Recibir datos desde la consola

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,9 +1,19 @@
-const { crearArchivoDeMultiplicar } = require("./helpers/multiplicar")
-const base = 6;
+const { crearArchivoDeMultiplicar } = require("./helpers/multiplicar");
 
 console.clear();
+
+// console.log(process.argv);
+
+const [ , , arg3 = "base=5"] = process.argv;
+const [ , base = 5 ] = arg3.split("=");
+console.log( base );
+
+
+// const base = 6;
 
 crearArchivoDeMultiplicar(base)
   .then(nombreArchivo => console.log(nombreArchivo, "creado"))
   .catch(err => console.log(err))
+
+
 
