507f26a8fd68c2c23209a54f0aa28eb1e8c97e6a
Khisus19 <khisus19@gmail.com>
2022-12-24 11:25:56 -0600

    Create helper dir

--- 04-app-node-tasklist/app.js
+++ 04-app-node-tasklist/app.js
@@ -1,1 +1,14 @@
 require("colors");
+
+const { mostrarMenu } = require("./helpers/mensajes")
+
+console.clear();
+
+const main = async () => {
+  console.log("Hola, mundo");
+
+  mostrarMenu();
+}
+
+main();
+
---
+++ 04-app-node-tasklist/helpers/mensajes.js
@@ -0,0 +1,18 @@
+require("colors");
+
+const mostrarMenu = () => {
+
+  console.clear();
+  console.log("================================");
+  console.log("     Seleccione una opción")
+  console.log("================================");
+
+
+
+
+
+}
+
+module.export = {
+  mostrarMenu
+}
