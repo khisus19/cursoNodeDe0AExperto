6e7ac72efe9affce36abce31632addde6be4eeb6
Khisus19 <khisus19@gmail.com>
2022-12-25 23:07:07 -0600

    Add readLine to receive inputs

--- 04-app-node-tasklist/app.js
+++ 04-app-node-tasklist/app.js
@@ -1,13 +1,20 @@
 require("colors");
 
-const { mostrarMenu } = require("./helpers/mensajes")
+const { mostrarMenu, pausa } = require("./helpers/mensajes");
 
 console.clear();
 
 const main = async () => {
   console.log("Hola, mundo");
 
-  mostrarMenu();
+  let opt = "";
+
+  do {
+    opt = await mostrarMenu();
+    console.log({ opt });
+    if (opt !== "0") await pausa();
+
+  } while( opt !== "0" );
 }
 
 main();
--- 04-app-node-tasklist/helpers/mensajes.js
+++ 04-app-node-tasklist/helpers/mensajes.js
@@ -1,18 +1,53 @@
 require("colors");
 
 const mostrarMenu = () => {
+  return new Promise(resolve => {
 
-  console.clear();
-  console.log("================================");
-  console.log("     Seleccione una opción")
-  console.log("================================");
+    console.clear();
+    console.log("================================");
+    console.log("     Seleccione una opción")
+    console.log("================================\n");
+  
+    console.log(`${ "1.".green } Crear tarea`);
+    console.log(`${ "2.".green } listar tareas`);
+    console.log(`${ "3.".green } listar tareas completadas`);
+    console.log(`${ "4.".green } listar tareas pendientes`);
+    console.log(`${ "5.".green } Completar tarea(s)`);
+    console.log(`${ "6.".green } Borrar tarea`);
+    console.log(`${ "7.".green } Crear tarea`);
+    console.log(`${ "0.".green } Salir\n`);
+  
+    const readLine = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+  
+    readLine.question("Seleccione una opción: ", (opt) => {
+      readLine.close();
+      resolve(opt);
+    })
 
-
-
-
+  })
 
 }
 
-module.export = {
-  mostrarMenu
+const pausa = () => {
+
+  return new Promise( resolve => {
+    const readLine = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout
+    }); 
+  
+    readLine.question(`\nPresione ${ "ENTER".green } para continuar\n`, (opt) => {
+      readLine.close();
+      resolve();
+    })
+
+  })
+}
+
+module.exports = {
+  mostrarMenu,
+  pausa
 }
